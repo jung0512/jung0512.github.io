d69c015ea40488e30e0b93f05f1386ebdf6ced5c
정원 <102035198+jung0512@users.noreply.github.com>
2023-03-24 11:33:03 +0900

    homepage

---
+++ script/script.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function(){
+    $(".main>li").mouseover(function(){
+        $(this).find(".sub").stop().slideDown()
+    })
+    .mouseout(function(){
+        $(this).find(".sub").stop().slideUp()
+    })
+})
